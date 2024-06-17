f97d5f72f2c3468356e64098b394376512581b8d
Krish Prasad <19krishprasad06@gmail.com>
2024-06-17 23:10:52 +0530

    Theme Changer

---
+++ 09themeSwitcher/src/context/theme.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React ,{createContext, useContext}from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
